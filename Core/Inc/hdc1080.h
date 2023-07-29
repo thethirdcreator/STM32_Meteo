717441f93007a4f32c77445c7319c91fc7ccae87
Th3rd <thethirdcreator@gmail.com>
2023-07-29 15:18:41 +0300

    - Частичный рефакторинг драйвера HDC1080

--- Core/Inc/hdc1080.h
+++ Core/Inc/hdc1080.h
@@ -4,26 +4,56 @@
 #include "stm32f0xx_hal.h"
 #include "stm32f0xx_hal_i2c.h"
 
+//#define bHDC1080UseFloatCalculation
+
 #define         HDC_1080_ADDR                            0x40
 #define         Configuration_register_addr              0x02
 #define         Temperature_register_addr                0x00
 #define         Humidity_register_addr                   0x01
 
+#define HDC1080_HRES 0x8
+#define HDC1080_TRES 0x10
+#define HDC1080_BTST 0x11
+#define HDC1080_MODE 0x12
+#define HDC1080_HEAT 0x13
+#define HDC1080_RST  0x15
 
-typedef enum
+
+typedef struct HDC1080_Instance
 {
-  Temperature_Resolution_14_bit = 0,
-  Temperature_Resolution_11_bit
-}Temp_Res;
+	I2C_HandleTypeDef hi2c;
+	enum
+	{
+	  Temperature_Resolution_14_bit,
+	  Temperature_Resolution_11_bit
+	}Temp_Res;
+	enum
+	{
+	  Humidity_Resolution_14_bit,
+	  Humidity_Resolution_11_bit,
+	  Humidity_Resolution_8_bit
+	}Humi_Res;
 
-typedef enum
-{
-  Humidity_Resolution_14_bit = 0,
-  Humidity_Resolution_11_bit = 1,
-  Humidity_Resolution_8_bit =2
-}Humi_Res;
+	unsigned int bUseHeater : 1;
+	unsigned int bAquireInSequence : 1;
 
-void hdc1080_init(I2C_HandleTypeDef* hi2c_x,Temp_Res Temperature_Resolution_x_bit,Humi_Res Humidity_Resolution_x_bit);
-uint8_t hdc1080_start_measurement(I2C_HandleTypeDef* hi2c_x,float* temperature, uint8_t* humidity);
+	int32_t temp;
+	uint32_t humi;
+
+};
+
+void hdc1080_init(struct HDC1080_Instance*);
+
+/*
+void hdc1080_init(I2C_HandleTypeDef*,Temp_Res Temperature_Resolution_x_bit,HDC1080_Humi_Res Humidity_Resolution_x_bit);
+#ifdef bHDC1080UseFloatCalculation
+uint8_t hdc1080_start_measurement(I2C_HandleTypeDef*,float*, uint8_t*);
+#else
+uint8_t hdc1080_start_measurement(I2C_HandleTypeDef*,int32_t*, uint8_t*);
+void hdc1080_get_t(I2C_HandleTypeDef*,int32_t*);
+void hdc1080_get_t(I2C_HandleTypeDef*,uint8_t*);
+#endif
+*/
+
 #endif
 
--- Core/Src/hdc1080.c
+++ Core/Src/hdc1080.c
@@ -1,14 +1,20 @@
 #include "hdc1080.h"
 
 
-void hdc1080_init(I2C_HandleTypeDef* hi2c_x,Temp_Res Temperature_Resolution_x_bit,Humi_Res Humidity_Resolution_x_bit)
+void hdc1080_init(struct HDC1080_Instance* HDC1080ptr)
 {
-	/* Temperature and Humidity are acquired in sequence, Temperature first
+	uint16_t control_Reg = 0;
+	control_Reg = (HDC1080ptr->Humi_Res<<HDC1080_HRES)|(HDC1080ptr->Temp_Res<<HDC1080_TRES)|(HDC1080ptr->Temp_Res<<HDC1080_TRES) ;
+}
+
+/*void hdc1080_init(I2C_HandleTypeDef* hi2c_x,HDC1080_Temp_Res Temperature_Resolution_x_bit,HDC1080_Humi_Res Humidity_Resolution_x_bit)
+{
+	 Temperature and Humidity are acquired in sequence, Temperature first
 	 * Default:   Temperature resolution = 14 bit,
 	 *            Humidity resolution = 14 bit
-	 */
 
-	/* Set the acquisition mode to measure both temperature and humidity by setting Bit[12] to 1 */
+
+	 Set the acquisition mode to measure both temperature and humidity by setting Bit[12] to 1
 	uint16_t config_reg_value=0x1000;
 	uint8_t data_send[2];
 
@@ -33,8 +39,17 @@ void hdc1080_init(I2C_HandleTypeDef* hi2c_x,Temp_Res Temperature_Resolution_x_bi
 	data_send[1]= (config_reg_value&0x00ff);
 
 	HAL_I2C_Mem_Write(hi2c_x,HDC_1080_ADDR<<1,Configuration_register_addr,I2C_MEMADD_SIZE_8BIT,data_send,2,1000);
-}
+}*/
 
+/*
+void hdc1080_get_t(I2C_HandleTypeDef* hi2c_x,int32_t* temperature)
+{
+
+}
+void hdc1080_get_t(I2C_HandleTypeDef* hi2c_x,uint8_t* humidity)
+{
+
+}
 
 uint8_t hdc1080_start_measurement(I2C_HandleTypeDef* hi2c_x,float* temperature, uint8_t* humidity)
 {
@@ -45,12 +60,12 @@ uint8_t hdc1080_start_measurement(I2C_HandleTypeDef* hi2c_x,float* temperature, 
 
 	HAL_I2C_Master_Transmit(hi2c_x,HDC_1080_ADDR<<1,&send_data,1,1000);
 
-	/* Delay here 15ms for conversion compelete.
+	 Delay here 15ms for conversion compelete.
 	 * Note: datasheet say maximum is 7ms, but when delay=7ms, the read value is not correct
-	 */
+
 	HAL_Delay(15);
 
-	/* Read temperature and humidity */
+	 Read temperature and humidity
 	HAL_I2C_Master_Receive(hi2c_x,HDC_1080_ADDR<<1,receive_data,4,1000);
 
 	temp_x =((receive_data[0]<<8)|receive_data[1]);
@@ -63,3 +78,4 @@ uint8_t hdc1080_start_measurement(I2C_HandleTypeDef* hi2c_x,float* temperature, 
 	return 0;
 
 }
+*/
